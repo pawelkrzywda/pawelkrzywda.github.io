7a803078eeefa7963e0e4ab4f4974966a3a65ec9
Pawel Krzywda <paw.krzywda@gmail.com>
2022-10-09 19:31:14 +0200

    Anoher minor fixes.

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
   const apiRoot = 'https://kodilla-tasks-1.herokuapp.com/v1/tasks';
-  const trelloApiRoot = 'https://kodilla-tasks-1.herokuapp.com/v1/tasks/trello';
+  const trelloApiRoot = 'https://kodilla-tasks-1.herokuapp.com/v1/trello';
   const datatableRowTemplate = $('[data-datatable-row-template]').children()[0];
   const $tasksContainer = $('[data-tasks-container]');
 
